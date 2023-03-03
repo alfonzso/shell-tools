de6c9f7e957172bc1d19d1f82c4e971336cf909a
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2023-03-03 21:58:21 +0100

    b64 minor fix

--- lib/b64_toggle.sh
+++ lib/b64_toggle.sh
@@ -3,9 +3,9 @@ function b64_toggle() {
   b64_regex='^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$'
   b64_decoded="$(echo $input_str | base64 -d 2> /dev/null)"
   echo "---------------------------sh"
-  if [[ $b64_decoded = *[![:ascii:]]* ]]; then
-    echo "$input_str" | base64
+  if [[ "$b64_decoded" = *[![:ascii:]]* ]]; then
+    echo "$input_str" | base64 -w0 && echo
   else
-    echo $b64_decoded
+    echo "$b64_decoded"
   fi
 }
