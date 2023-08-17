f9174d65387081b0d089230355103670fd8e3799
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2023-08-18 00:56:29 +0200

    b64 multiline

--- lib/b64_toggle.test.sh
+++ lib/b64_toggle.test.sh
@@ -1,5 +1,9 @@
-source b64_toggle.sh
+#! /bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+ROOT=$DIR
 
+source $ROOT/b64_toggle.sh
+# set -x
 readonly testVars=(
 'fafafa|ZmFmYWZhCg=='
 'fafa fefe lkeke|ZmFmYSBmZWZlIGxrZWtlCg=='
@@ -9,13 +13,18 @@ readonly testVars=(
 'nexus-go:Almafaxa_0012123|bmV4dXMtZ286QWxtYWZheGFfMDAxMjEyMwo='
 'ejRtQ05CV2tPd0Y4T1VoMwo=|z4mCNBWkOwF8OUh3'
 'z4mCNBWkOwF8OUh3|ejRtQ05CV2tPd0Y4T1VoMwo='
+'keke\nfefe\nlele\n|a2VrZQpmZWZlCmxlbGUK'
+'{ "auths": { "dockerhub.lol": { "auth": "faf" }, "dockerhub-faf.hu": { "auth": "fef" } } }|eyAiYXV0aHMiOiB7ICJkb2NrZXJodWIubG9sIjogeyAiYXV0aCI6ICJmYWYiIH0sICJkb2NrZXJodWItZmFmLmh1IjogeyAiYXV0aCI6ICJmZWYiIH0gfSB9Cg=='
+'{\n   "auths":{\n      "dockerhub.lol":{\n         "auth":"faf"\n      },\n      "dockerhub-faf.hu":{\n         "auth":"fef"\n      }\n   }\n}|ewogICAiYXV0aHMiOnsKICAgICAgImRvY2tlcmh1Yi5sb2wiOnsKICAgICAgICAgImF1dGgiOiJmYWYiCiAgICAgIH0sCiAgICAgICJkb2NrZXJodWItZmFmLmh1Ijp7CiAgICAgICAgICJhdXRoIjoiZmVmIgogICAgICB9CiAgIH0KfQo='
+'ewogICAiYXV0aHMiOnsKICAgICAgImRvY2tlcmh1Yi5sb2wiOnsKICAgICAgICAgImF1dGgiOiJmYWYiCiAgICAgIH0sCiAgICAgICJkb2NrZXJodWItZmFmLmh1Ijp7CiAgICAgICAgICJhdXRoIjoiZmVmIgogICAgICB9CiAgIH0KfQo|{\n   "auths":{\n      "dockerhub.lol":{\n         "auth":"faf"\n      },\n      "dockerhub-faf.hu":{\n         "auth":"fef"\n      }\n   }\n}'
 )
 
 export TESTING=1
 for fields in "${testVars[@]}"
 do
     IFS=$'|' read -r "source" "expected" <<< "$fields"
-    b64_result="$(b64_toggle "$source" )"
-    [ "$b64_result" = "$expected" ] && { echo "[OK] $source " ;} || { echo "!!! NOK !!! $source -> $b64_result -> $expected" && exit 1; }
+    b64_result="$(b64_toggle "$( echo -e "$source" )" )"
+    [ "$b64_result" = "$( echo -e "$expected" )" ] && { echo "[OK] $source " ;} || { echo "!!! NOK !!! $source -> $b64_result -> $expected" && exit 1; }
+    # [ "$b64_result" = "$expected" ] && { echo "[OK] $source " ;} || { echo "!!! NOK !!! $source -> $b64_result -> $expected" && exit 1; }
     # [ "$b64_result" = "$expected" ] && { echo "[OK] $source " ;} || { echo "!!! NOK !!! $source -> $b64_result -> $expected"  ; }
 done
--- tst.sh
+++ tst.sh
@@ -1,3 +1,5 @@
-for i in keke fefe sasa; do
-  alias $i="echo $i"
-done
+# for i in keke fefe sasa; do
+#   alias $i="echo $i"
+# done
+
+bash lib/b64_toggle.test.sh
