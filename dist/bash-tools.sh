e0c44753e321350e32842ab44381dfe1e23d782c
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2023-08-18 00:59:49 +0200

    not builded ...

--- dist/bash-tools.sh
+++ dist/bash-tools.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
-# version: a07918f
+# version: f9174d6
 
 function b64_toggle() {
+  [[ -n $SH_TOOLS_DEBUG ]] && set -x
   input_str=$1
   b64_regex='^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$'
-  b64_decoded="$(echo $input_str | base64 -d 2> /dev/null)"
+  b64_decoded="$(echo """$input_str""" | base64 -d 2> /dev/null)"
   [[ -z "$TESTING" ]] && echo "---------------------------sh"
   res=false; [[ "$b64_decoded" =~ "$b64_regex" ]] && res=true
   [[ -z "$TESTING" ]] && echo $res
-  if [[ "$b64_decoded" = *[![:ascii:]]* ]]; then
+  if [[ "$b64_decoded" = *[![:ascii:]]* || "$b64_decoded" = "" ]]; then
     echo "$input_str" | base64 -w0 && echo
   else
     echo "$b64_decoded"
   fi
+  [[ -n $SH_TOOLS_DEBUG ]] && set +x
 }
 
 function list_remoteless_local_branches() {
@@ -30,7 +32,7 @@ function self_update() {
 }
 
 function wellcome() {
-  echo " .... Shell-Tools .... vers: a07918f "
+  echo " .... Shell-Tools .... vers: f9174d6 "
 }
 
 alias ll='ls -alF'
